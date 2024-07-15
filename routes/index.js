b566e0e79f6c3a84d280ad32fb5273b80a045241
mifta <rizqhaamifta@gmail.com>
2024-07-15 18:31:49 +0700

    menambahkan rute beranda

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const { findAllPerpustakaan, findPerpustakaanById, createNewPerpustakaan } = require('../controller/perpustakaanControllers');
 
+//ANCHOR Inisialisasi penggunaan router
 const router = require('express').Router();
 
+//ANCHOR - Rute beranda
+router.get('/', (req, res) => {
+    res.send('Welcome guys to express!');
+});
+
+
+//ANCHOR - Rute perpustakaan
 router.get('/perpustakaan', findAllPerpustakaan);
 router.get('/perpustakaan/:id', findPerpustakaanById); 
 router.post('/perpustakaan', createNewPerpustakaan);
+
 
 module.exports = router
