fbc91e68afd26f43549faad1f80abe8d0033d7e4
mifta <rizqhaamifta@gmail.com>
2024-07-15 17:14:08 +0700

    memperbaharui penempatan struktur code

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 //SECTION - Configuration Express
+
 //ANCHOR - inisialized express
 const express = require('express');
 const app = express();
 
-//ANCHOR - Middleware untuk mem-parsing body JSON
-app.use(express.json());
-
 //ANCHOR - port configuration
 const port = 3000;
 
+//ANCHOR - Middleware untuk mem-parsing body JSON
+app.use(express.json());
+
+
+//SECTION - routes
 //ANCHOR - import router
 const router = require('./routes/index');
 app.use(router);
-
-
-
-app.get('/', (req, res) => {
-    res.send('Welcome guys to express!');
-});
 
 app.listen(port, () => console.log(`server listening on ${port}`));
