892650d0073095d8da7f07f9be61d8c110c41baa
mifta <rizqhaamifta@gmail.com>
2024-07-16 19:58:33 +0700

    membuat migration untuk library

---
+++ migrations/20240715113019-create-library.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Libraries', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      library_id: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      library_name: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      employee_id: {
+        type: Sequelize.INTEGER,
+        unique: true
+      },
+      isbn: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Libraries');
+  }
+};
