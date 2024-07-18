4e213485348b086263923228bfc3a29853deb6aa
mifta <rizqhaamifta@gmail.com>
2024-07-18 18:51:59 +0700

    menambahkan routes admin, membe, dan books. Kemudian menambahkan sedikit controller untuk member routes

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,12 @@
 const { authenticate, authorize } = require('../middleware/authorization');
-const {
-    findAllLibrary,
-    findLibraryById,
-    createNewLibrary,
-    updateLibrary,
-    deleteLibrary
-} = require('../controller/libraryControllers');
 
-const {
-    findAllUser,
-    findUserById,
-    registerUser,
-    loginUser,
-    updateUser,
-    deleteUser
-} = require('../controller/userControllers');
+const { findAllLibrary, findLibraryById, createNewLibrary, updateLibrary, deleteLibrary } = require('../controller/libraryControllers');
+
+const { findAllAdmin, findAdminById, createAdmin, loginAdmin, updateAdmin, deleteAdmin } = require('../controller/adminControllers');
+
+const { createMember, loginMember, findAllMember, findMemberById, updateMember, deleteMember } = require('../controller/memberControllers');
+
+const { createBook, findAllBook, findBookById, updateBook, deleteBook } = require('../controller/bookControllers');
 
 //ANCHOR Inisialisasi penggunaan router
 const router = require('express').Router();
@@ -25,21 +17,37 @@ router.get('/', (req, res) => {
 });
 
 
-//ANCHOR - Library Routes
+//ANCHOR - Library Routes 
+//NOTE - Super Admin memiliki akses penuh pada Routes Library
 router.get('/perpustakaan', authenticate, authorize(['admin']), findAllLibrary);
 router.get('/perpustakaan/:id', authenticate, authorize(['admin']), findLibraryById);
-router.post('/perpustakaan', authenticate, authorize(['admin']), createNewLibrary);
-router.patch('/perpustakaan/:id', authenticate, authorize(['admin']), updateLibrary);
-router.delete('/perpustakaan/:id', authenticate, authorize(['admin']), deleteLibrary);
+router.post('/perpustakaan', createNewLibrary);
+router.patch('/perpustakaan/:id', updateLibrary);
+router.delete('/perpustakaan/:id', deleteLibrary);
 
-//ANCHOR - User Routes
-router.get('/user', authenticate, authorize(['admin']), findAllUser);
-router.get('/user/:id', authenticate, authorize(['admin']), findUserById);
-router.post('/user/register', authenticate, authorize(['admin']), registerUser);
-router.post('/user/login', loginUser);
-router.patch('/user/:id', authenticate, authorize(['admin']), updateUser);
-router.delete('/user/:id', authenticate, authorize(['admin']), deleteUser);
+//ANCHOR - Admin Routes
+router.get('/admin', authenticate, authorize(['admin']), findAllAdmin);
+router.get('/admin/:id', authenticate, authorize(['admin']), findAdminById);
+router.post('/admin/create-admin', createAdmin); //NOTE - Super Admin bisa membuat admin
+router.post('/admin/login-admin', loginAdmin);
+router.patch('/admin/:id', authenticate, authorize(['admin']), updateAdmin); //NOTE - Super Admin bisa mengupdate admin juga
+router.delete('/admin/:id', authenticate, deleteAdmin); //NOTE - Hanya super admin yang dapat menghapus admin
 
+// ANCHOR Members
+router.get('/member', findAllMember);
+router.get('/member/:id', findMemberById);
+router.post('/member/create-member', createMember);
+router.post('/member/login-member', loginMember);
+router.patch('/member/update-member/:id', updateMember);
+router.delete('/member/:id', deleteMember);
+
+
+//ANCHOR - Books
+router.get('/book', findAllBook);
+router.get('/book/:id', findBookById);
+router.post('/book/create-book', createBook);
+router.patch('/book/:id', updateBook);
+router.delete('/book/:id', deleteBook);
 
 
 
