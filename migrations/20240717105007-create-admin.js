c5ee56bb14d547a599c5df0e9dd086593b9aae01
mifta <rizqhaamifta@gmail.com>
2024-07-18 18:49:28 +0700

    membuat admin migrations

---
+++ migrations/20240717105007-create-admin.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Admins', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      employee_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Libraries', // Nama tabel yang direferensikan
+          key: 'employee_id' // Kolom yang direferensikan
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.ENUM('admin'),
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Admins');
+  }
+};
