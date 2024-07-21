030dd5b89c9cfca01c2c0094b11bb20e9c39ed29
mifta <rizqhaamifta@gmail.com>
2024-07-21 18:17:52 +0700

    menginisialisasi member_id pada model members

--- models/member.js
+++ models/member.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Member.init({
+    member_id: DataTypes.STRING,
     name: DataTypes.STRING,
     address: DataTypes.STRING,
     number_phone: DataTypes.STRING,
