c5af8deb4659bf22bbf86f5db09ff009197918f8
mifta <rizqhaamifta@gmail.com>
2024-07-24 23:35:56 +0700

    menambahkan controller get all book collection

--- controller/bookCollectionControllers.js
+++ controller/bookCollectionControllers.js
@@ -1,5 +1,68 @@
 const { Book, BookCollection, Library } = require('../models');
 
+const getAllBooksCollection = async (req, res, next) => {
+    try {
+        const data = await BookCollection.findAll();
+        const result = {
+            status: 'ok',
+            message: 'data get successfully',
+            data: data
+        }
+        if (data === undefined && data === null) {
+            return res.status(404).json({ status: 'failed', message: `data Admin with id ${id} is not found` })
+        }
+        res.json(result);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const getAllLibraryByLibraryId = async (req, res, next) => {
+    try {
+        const employeeId = req.user.employee_id;
+
+        if (!employeeId) {
+            return res.status(404).json({
+                status: 'failed',
+                message: 'Access denied. You do not have the required permissions to view libraries. '
+            });
+        }
+
+        //NOTE Temukan library berdasarkan employee_id
+        const library = await Library.findOne({ where: { employee_id: employeeId } });
+
+        if (!library) {
+            return res.status(404).json({
+                status: 'failed',
+                message: `Library not found for employee with ID ${employee_id}`
+            });
+        }
+
+        const { library_id } = library;
+
+        // Temukan semua book collection berdasarkan library_id
+        const data = await BookCollection.findAll({ where: { library_id } });
+
+        if (data.length === 0) {
+            return res.status(404).json({
+                status: 'failed',
+                message: `No book collections found for library ID ${library_id}`
+            });
+        }
+
+        const result = {
+            status: 'ok',
+            message: 'Data retrieved successfully',
+            data: data
+        };
+
+        res.json(result);
+
+    } catch (error) {
+        next(error);
+    }
+}
+
 const createBookCollection = async (req, res, next) => {
     try {
         const { book_collection_id, isbn, status } = req.body;
@@ -39,4 +102,4 @@ const createBookCollection = async (req, res, next) => {
     }
 }
 
-module.exports = { createBookCollection }
+module.exports = { createBookCollection, getAllBooksCollection, getAllLibraryByLibraryId }
