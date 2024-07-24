479aad0bc11077ddaa47189c2524c9de3f957297
mifta <rizqhaamifta@gmail.com>
2024-07-24 23:56:01 +0700

    menambahkan asosiasi

--- models/library.js
+++ models/library.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Library.hasMany(models.BookCollection, {
+        foreignKey: 'library_id',
+        as: 'bookCollection'
+      });
     }
   }
   Library.init({
--- models/member.js
+++ models/member.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Member.hasMany(models.Borrowing, {
+        foreignKey: 'member_id',
+        sourceKey: 'member_id',
+        as: 'borrowing'
+      });
     }
   }
   Member.init({
