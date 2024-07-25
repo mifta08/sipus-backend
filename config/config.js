22e21010f083e205da62bd9142a98ceccca7ebe7
mifta <rizqhaamifta@gmail.com>
2024-07-26 03:37:52 +0700

    menubah inisialisasi env

--- config/config.js
+++ config/config.js
@@ -35,10 +35,11 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     dialect: 'mysql',  // Ubah menjadi 'postgres' jika menggunakan PostgreSQL
     logging: false,
   },
