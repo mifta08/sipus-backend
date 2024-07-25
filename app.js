079ddc837eac15a99b794a286e2a9c662fa3a741
mifta <rizqhaamifta@gmail.com>
2024-07-26 06:57:51 +0700

    menambahkan pengecekan koneksi db

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const { sequelize } = require('./models');
 
 // Import database configuration
 // const sequelize = require('./config/db');
@@ -22,4 +23,12 @@ app.use(router);
 const errorHandler = require('./middleware/errorHandler');
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`server listening on ${port}`));
+sequelize.authenticate()
+    .then(() => {
+        console.log('Database connected successfully.');
+        app.listen(port, () => console.log(`Server listening on port ${port}`));
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+        process.exit(1); // Exit with error code
+    });
