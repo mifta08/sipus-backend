5b7c955e6952289fcb8997fdf741a9c007b3a2de
mifta <rizqhaamifta@gmail.com>
2024-07-26 03:09:09 +0700

    mengubah ektensi ke js untuk konfigurasi env

---
+++ config/config.js
@@ -0,0 +1,45 @@
+//ANCHOR - Local configure
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "sipus",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "sipus",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
+
+require('dotenv').config();  // Memuat variabel lingkungan dari file .env
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || 'sipus',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',  // Ubah menjadi 'postgres' jika menggunakan PostgreSQL
+    logging: false,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',  // Ubah menjadi 'postgres' jika menggunakan PostgreSQL
+    logging: false,
+  },
+};
