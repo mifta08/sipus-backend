73c4968e7da13385802fe52c375bb93c77169f84
mifta <rizqhaamifta@gmail.com>
2024-07-25 14:46:07 +0700

    menambahkan dotenv dan script

--- models/index.js
+++ models/index.js
@@ -13,7 +13,16 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    process.env.DB_NAME || config.database,
+    process.env.DB_USER || config.username,
+    process.env.DB_PASSWORD || config.password,
+    {
+      host: process.env.DB_HOST || config.host,
+      dialect: 'mysql',
+      ...config
+    }
+  );
 }
 
 fs
