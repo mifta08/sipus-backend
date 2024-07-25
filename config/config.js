e29842f6c98b1175291141211072cad83c329353
mifta <rizqhaamifta@gmail.com>
2024-07-26 04:21:03 +0700

    menambahkan port

--- config/config.js
+++ config/config.js
@@ -39,7 +39,7 @@ module.exports = {
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
+    port: process.env.MYSQL_PORT || 3306,
     dialect: 'mysql',  // Ubah menjadi 'postgres' jika menggunakan PostgreSQL
     logging: false,
   },
